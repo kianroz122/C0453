81a953c63b5dbf3877ea7ddc1f77572a067a5a63
kianroz122 <50154724+kianroz122@users.noreply.github.com>
2021-02-04 14:19:14 +0000

    making a working version of the app

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -9,6 +9,8 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public const int FEET_IN_MILES = 5280;
+
         private double miles;
 
         private double feet;
@@ -18,17 +20,27 @@ namespace ConsoleAppProject.App01
         /// </summary>
         public void Run()
         {
+            OutputHeading();
             InputMiles();
             CalculateFeet();
             OutputFeet();
         }
+
+        private void OutputHeading()
+        {
+            Console.WriteLine("\n------------------------");
+            Console.WriteLine("--Convert Miles to feet-");
+            Console.WriteLine("-----BY Kian Rozblat----");
+            Console.WriteLine("------------------------\n");
+        }
+
         /// <summary>
         /// Promt the user to enter the distance in miles 
         /// Input the miles as a double number
         /// </summary>
         private void InputMiles()
         {
-            Console.Write("please enter a number of miles");
+            Console.Write("please enter a number of miles ");
             string value = Console.ReadLine();
             miles = Convert.ToDouble(value);
         }
@@ -37,14 +49,15 @@ namespace ConsoleAppProject.App01
         /// </summary>
         private void CalculateFeet()
         {
-
+            feet = miles * FEET_IN_MILES;
         }
 
         /// <summary>
         /// Outputs the calculated feet to the user 
         /// </summary>
         private void OutputFeet()
-        { 
+        {
+            Console.WriteLine(miles + " miles is " + feet + " feet");
 
         }
     }
