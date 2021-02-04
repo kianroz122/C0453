6572ce0b0ed9509751d19a7f0a31ec6e12425046
kianroz122 <50154724+kianroz122@users.noreply.github.com>
2021-02-04 13:44:45 +0000

    added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -4,10 +4,43 @@
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Kian Rozblat Version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
 
+        private double feet;
+
+        /// <summary>
+        /// Runs the method of converting
+        /// </summary>
+        public void Run()
+        { 
+        
+        }
+        /// <summary>
+        /// Promt the user to enter the distance in miles 
+        /// Input the miles as a double number
+        /// </summary>
+        private void InputMiles()
+        {
+
+        }
+        /// <summary>
+        /// Calculates the Inputted miles into feet
+        /// </summary>
+        private void CalculateFeet()
+        {
+
+        }
+
+        /// <summary>
+        /// Outputs the calculated feet to the user 
+        /// </summary>
+        private void OutputFeet()
+        { 
+
+        }
     }
 }
