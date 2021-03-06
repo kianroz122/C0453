83382d5b7efac11b5fa741be9c169770fb9c4dd9
kianroz122 <50154724+kianroz122@users.noreply.github.com>
2021-03-06 12:48:32 +0000

    app03

--- ConsoleAppProject/App03/GradeCalculater.cs
+++ ConsoleAppProject/App03/GradeCalculater.cs
@@ -6,10 +6,20 @@ namespace ConsoleAppProject.App03
 {
     public class StudentGrades
     {
+        //constants
+        public const int LowestMark = 0;
+        public const int LowestGradeD = 40;
+        public const int LowestGradeC = 50;
+        public const int LowestGradeB = 60;
+        public const int LowestGradeA = 70;
+        public const int HighestMark = 100;
+
+
+        //properties
         public string[] Students;
         public int[] Marks;
         public int[] GradeProfile;
-
+        public int[] StatsMarks;
 
         public double Mean;
         public int Minimum;
@@ -54,7 +64,7 @@ namespace ConsoleAppProject.App03
         /// </summary>
         public Grades ConvertToGrade(int mark)
         {
-            if (mark >= 0 && mark < 40)
+            if (mark >= 0 && mark < LowestGradeD)
                 return Grades.F;
             else
             return Grades.X;
@@ -65,7 +75,18 @@ namespace ConsoleAppProject.App03
         /// </summary>
         public void CalculateStats()
         {
-            throw new NotImplementedException();
+            Minimum = Marks[0];
+            Maximum = Marks[0];
+
+            double total = 0;
+            
+            foreach(int mark in Marks)
+            {
+                if (mark > Maximum) Maximum = mark;
+                if (mark < Minimum) Minimum = mark;
+                total = total + mark;
+            }
+            Mean = total / Marks.Length;
         }
 
         /// <summary>
--- TestProject/TestStudentGrades.cs
+++ TestProject/TestStudentGrades.cs
@@ -8,6 +8,24 @@ namespace TestProject
     {
         private readonly StudentGrades converter = new StudentGrades();
 
+        private readonly int[] testMarks;
+        private readonly int[] StatsMarks;
+
+
+        public TestStudentGrades()
+        {
+            testMarks = new int[]
+            {
+                10,20,30,40,50,60,70,80,90,100
+            };
+
+            StatsMarks = new int[]
+            {
+                10,20,30,40,50,60,70,80,90,100
+             };
+
+        }
+
         [TestMethod]
         public void TestConvert0ToGradeF()
         {
@@ -19,6 +37,41 @@ namespace TestProject
 
             //assert
             Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert39ToGradef()
+        {
+            //arrange
+            Grades expectedGrade = Grades.F;
+
+            //act
+            Grades actualGrade = converter.ConvertToGrade(39);
+
+            //assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestCalculationMean()
+        {
+            converter.Marks = testMarks;
+            double expectedMean = 55.0;
+            converter.CalculateStats();
+
+            Assert.AreEqual(expectedMean, converter.Mean);
+        }
+
+        [TestMethod]
+        public static void TestCalculateMin()
+        {
+
+            StudentGrades.Marks = StatsMarks;
+            int expectedMin = 10;
+
+            StudentGrades.CalculateStats();
+
+            Assert.AreEqual(expectedMin, StudentGrades.Minimum);
         }
     }
 }
